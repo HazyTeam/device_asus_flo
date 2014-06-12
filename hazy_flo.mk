8e64fea53df60112e6bb76e5fec9d2c5864e7f95
Francesco Colella <mnemonyc@hotmail.com>
2014-06-12 20:28:23 +0200

    Added Hazy vendor dependencies

--- hazy_flo.mk
+++ hazy_flo.mk
@@ -42,5 +42,6 @@ TARGET_SELINUX_CONFIG := flo_defconfig
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/flo/device.mk)
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
+$(call inherit-product, vendor/twixkat/device-partial.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
 
